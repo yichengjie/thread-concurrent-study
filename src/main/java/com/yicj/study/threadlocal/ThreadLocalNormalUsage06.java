2c448f2ba568e0b3fa372d05f42956abc53f03fe
易成杰 <626659321@qq.com>
2020-06-23 20:49:37 +0800

    保存

---
+++ src/main/java/com/yicj/study/threadlocal/ThreadLocalNormalUsage06.java
@@ -0,0 +1,61 @@
+package com.yicj.study.threadlocal;
+
+/**
+ * ClassName: ThreadLocalNormalUsage06
+ * Description: TODO(描述)
+ * Date: 2020/6/23 20:42
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+/**
+ * 描述：     演示ThreadLocal用法2：避免传递参数的麻烦
+ */
+public class ThreadLocalNormalUsage06 {
+
+    public static void main(String[] args) {
+        new Service1().process("test");
+    }
+}
+
+class Service1 {
+
+    public void process(String name) {
+        User user = new User("超哥");
+        UserContextHolder.holder.set(user);
+        new Service2().process();
+    }
+}
+
+class Service2 {
+
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        ThreadSafeFormatter.dateFormatThreadLocal.get();
+        System.out.println("Service2拿到用户名：" + user.name);
+        new Service3().process();
+    }
+}
+
+class Service3 {
+
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("Service3拿到用户名：" + user.name);
+        UserContextHolder.holder.remove();
+    }
+}
+
+class UserContextHolder {
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+}
+
+class User {
+
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
