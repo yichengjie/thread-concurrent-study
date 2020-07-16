077f96bb9041660eb096af9f6962682b52c3139d
易成杰 <626659321@qq.com>
2020-07-16 17:43:57 +0800

    生产者消费者

---
+++ src/main/java/com/yicj/study/producerconsumer/BlockingQueueChannel.java
@@ -0,0 +1,13 @@
+package com.yicj.study.producerconsumer;
+
+/**
+ * ClassName: BlockingQueueChannel
+ * Description: TODO(描述)
+ * Date: 2020/7/16 17:13
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class BlockingQueueChannel {
+}
